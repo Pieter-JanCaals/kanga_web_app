30dc274b43a0db8cbf00d3f49b29510ab01b0b94
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-25 16:22:06 -0500

    Initial commit with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :events, only [:index, :show] do
+    resources :drinks, only [:index]
+    resources :order_drinks, only [:create]
+  end
+
+  resources :orders, except [:index, :create, :new]
+
 end
