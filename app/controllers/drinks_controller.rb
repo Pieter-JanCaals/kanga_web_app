ab9e6b0ccc5d27bf3a1eb1b4d34d6496282620a3
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-26 16:39:38 -0500

    started implementing logic for displaying drinks

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,4 +1,24 @@
 class DrinksController < ApplicationController
   def index
+    event = Event.find(params[:id])
+    bar = closest_bar(event)
+    if params[:category].is_set?
+      category = Category.find(params[:category])
+      @drinks = bar.drinks.where(category: category)
+    else
+      @drinks = popular_drinks(bar.drinks)
+    end
+  end
+
+  private
+
+  def closest_bar(event)
+    # logic to find closest bar to user
+    event.bars.first
+  end
+
+  def popular_drinks(drinks)
+    # logic to determine popular drinks
+    drinks
   end
 end
