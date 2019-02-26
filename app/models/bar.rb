8101d2abbbdfd16d2c0e108589a76c0b2606abfb
frederic charest <frederic.charest.42@hotmail.com>
2019-02-26 16:34:38 -0500

    seed with Events, Categories, Drinks, Bars, Bar_drinks and Users

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,6 +1,6 @@
 class Bar < ApplicationRecord
   belongs_to :event
   has_many :orders
-  has_many :bar_drinks
+  has_many :bar_drinks, dependent: :destroy
   has_many :drinks, through: :bar_drinks
 end
