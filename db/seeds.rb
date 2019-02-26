ebf9e2dc8ad1bdda79cfb0d767ac5f4e1342bded
frederic charest <frederic.charest.42@hotmail.com>
2019-02-26 14:09:16 -0500

    drinks added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,13 @@
 require "yaml"
 # require "pry-byebug"
 
+# -- Setting the hashes that will contains an instance of all objects --
+events_hash = {}
+categories_hash = {}
+drinks_hash = {}
+
+
+# -- Creating methods for the two most common messages --
 def message_creating(data)
   print "Creating #{data}..."
 end
@@ -9,24 +16,23 @@ def message_done
   puts "Done!"
 end
 
-# -- Sets the yaml file to the variable "seed" --
+# -- Opening the yaml file and assigning it to the variable "seed" --
 filepath = File.dirname(__FILE__)
 filepath += "/seed.yaml"
 seed = YAML.load_file(filepath)
 
+
 # -- Deleting all databases content --
 print "Cleaning database..."
-
 Event.destroy_all
 Category.destroy_all
-
 message_done
+
 
 # -- Creating the events --
 message_creating("events")
-
 seed["events"].each do |event|
-  Event.create!(
+  temp_event = Event.create!(
     name: event["name"],
     longitude: event["longitude"],
     latitude: event["latitude"],
@@ -34,23 +40,34 @@ seed["events"].each do |event|
     start_date: event["start_date"],
     end_date: event["end_date"]
     )
+  events_hash[event["name"]] = temp_event
 end
-
 message_done
 
 
 # -- Creating the categories --
 message_creating("categories")
-categories_array = []
 seed["categories"].each do |category|
-  categories_array << category["name"]
-  Category.create!(
+  temp_category = Category.create!(
     name: category["name"]
     )
+  categories_hash[category["name"]] = temp_category
 end
 message_done
 
 
+# -- Creating the drinks --
+message_creating("drinks")
+seed["drinks"].each do |drink|
+  temp_drink = Drink.create!(
+    name: drink["name"],
+    prep_time: drink["prep_time"],
+    category: categories_hash[drink["category"]]
+    )
+  drinks_hash[drink["name"]] = temp_drink
+end
+message_done
 
-# All done!
+
+# -- All done! --
 puts "Finished!!!"
