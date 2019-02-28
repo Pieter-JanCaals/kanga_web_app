83f11a92a6de39db868be462121293f00594138b
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-02-28 14:08:15 -0500

    started drawing map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,15 +1,47 @@
 import mapboxgl from 'mapbox-gl';
 
+const fitMapToMarkers = (map, markers) => {
+  const bounds = new mapboxgl.LngLatBounds();
+  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+  map.fitBounds(bounds, { padding: 100, maxZoom: 100 })
+}
+
+const addMarkersToMap = (map, markers) => {
+  markers.forEach((marker) => {
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url(${marker.image_url})`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '70px';
+    element.style.height = '70px';
+
+    new mapboxgl.Marker(element)
+    .setLngLat([marker.lng, marker.lat])
+    .setPopup(new mapboxgl.Popup({ offset: 25, closeOnClick: false }) // add popups
+    .setHTML(marker.infoWindow))
+    .addTo(map);
+  })
+}
+
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
 
-  if (mapElement) { // only build a map if there's a div#map to inject into
+  let markers = JSON.parse(mapElement.dataset.markers);
+
+  if (mapElement) {
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v10'
+      style: 'mapbox://styles/mapbox/streets-v10',
+      center: [markers[0].lng, markers[0].lat]
     });
+    markers.shift();
+
+    console.log(markers)
+    addMarkersToMap(map, markers);
+    fitMapToMarkers(map, markers);
   }
 };
+
 
 export { initMapbox };
