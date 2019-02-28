bbfdf79eeff6acde78fe14222ffed8eb593406f0
frederic charest <frederic.charest.42@hotmail.com>
2019-02-28 17:56:16 -0500

    added logo to DB, seed and event index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_154057) do
+ActiveRecord::Schema.define(version: 2019_02_28_223927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2019_02_28_154057) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.string "photo"
+    t.string "logo"
   end
 
   create_table "order_drinks", force: :cascade do |t|
