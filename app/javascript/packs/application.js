17387608bade35139332fd85446b623b14fd4c0f
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-03-01 09:58:03 -0500

    Almost finished confirmed page but map is acting strange

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,5 @@
 import "bootstrap";
-import { initMapbox } from "../plugins/init_mapbox"
 
-initMapbox();
 
 
 
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,7 +3,7 @@ import mapboxgl from 'mapbox-gl';
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 25, maxZoom: 25 })
+  map.fitBounds(bounds, { padding: 70, maxZoom: 25 })
 }
 
 const addMarkersToMap = (map, markers) => {
