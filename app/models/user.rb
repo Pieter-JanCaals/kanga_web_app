398d6940487515b34fec4b043b94b10317a719d6
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-03-04 11:12:16 -0500

    Removed unused lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,4 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :friends, through: :friendships
-  # has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-  # has_many :inverse_friends, through: :inverse_friendships, source: :contact
 end
