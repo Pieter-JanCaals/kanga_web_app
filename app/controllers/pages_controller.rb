61a1d05e2f4a2c88501c151a48a445a72b389bdd
frederic charest <frederic.charest.42@hotmail.com>
2019-03-04 16:35:31 -0500

    Auto generate user functionality integrated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:splash]
 
   def splash
-
+    # -- The following line creates a new user as soon as they
+    #    reach the splash page if they are not already signed in.
+    #    Comment it out if you want to revert to the default
+    #    login behavior. --
+    sign_in User.create_demo_user unless user_signed_in?
   end
 end
