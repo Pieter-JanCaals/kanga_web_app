1fa5da1d3ca8a1089a02159697772fae7755c438
Jerome Kastoun <jeromekastoun@gmail.com>
2019-03-04 17:50:57 -0500

    Created confirmation page that is accessed by clicking on the barcode

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,22 @@ class PagesController < ApplicationController
   def splash
 
   end
+  def notification
+    confirmed_order = current_user.orders.find_by(status: "confirmed")
+    @qr = RQRCode::QRCode.new(confirmed_order.qr_code, size: 2, level: :h)
+    if confirmed_order
+
+      @markers =
+        [
+          { lng: confirmed_order.event.longitude, lat: confirmed_order.event.latitude },
+          {
+            lng: confirmed_order.bar.longitude, lat: confirmed_order.bar.latitude,
+            infoWindow: render_to_string(partial: "events/infowindow", locals: { bar: confirmed_order.bar }),
+            image_url: helpers.asset_url('logo.png')
+          }
+        ]
+    else
+      redirect_to drinks_path
+    end
+  end
 end
