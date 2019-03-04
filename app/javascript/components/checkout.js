bd969e89762232062a8045ff0109c2de415b8fd4
frederic charest <frederic.charest.42@hotmail.com>
2019-03-04 12:12:26 -0500

    commit to clear tree, nothing is working yet

--- app/javascript/components/checkout.js
+++ app/javascript/components/checkout.js
@@ -1,3 +1,6 @@
+// import { dinero } from 'dinero.js';
+import Dinero from 'dinero.js';
+
 // -- Initialize DOM elements --
 const tipAmount = document.querySelector("#tip-amount");
 const grandTotal = document.querySelector("#grand-total");
@@ -5,38 +8,43 @@ const minus = document.querySelector(".drink-minus-btn");
 const plus = document.querySelector(".drink-plus-btn");
 
 // -- Initialize frequently used variables --
-const initTotalValue = Math.round(parseFloat(document.querySelector("#grand-total").innerHTML.substr(1)) * 100) / 100;
-let tipPercent = 15;
-let tipTotal = Math.round((initTotalValue * (parseFloat(tipPercent) / 100)) * 100) / 100;
+// const initTotalValue = Math.round(parseFloat(document.querySelector("#grand-total").innerHTML.substr(1)) * 100) / 100;
+// const initTotalValue = Dinero( { amount: (grandTotal.innerHTML.substr(1)) * 100 } )
+// const initTotalValue = Dinero({ amount: 1000 });
+// console.log(initTotalValue.toFormat());
+// let tipPercent = 15;
+// let tipTotal = Math.round((initTotalValue * (parseFloat(tipPercent) / 100)) * 100) / 100;
 
 // -- Then, a buch of stuff happens... --
-const recalculateTipTotal = () => {
-  tipTotal = Math.round((initTotalValue * (parseFloat(tipPercent) / 100)) * 100) / 100;
-  tipAmount.innerHTML = `$${tipTotal}`;
-};
+// const recalculateTipTotal = () => {
+//   tipTotal = Math.round((initTotalValue * (parseFloat(tipPercent) / 100)) * 100) / 100;
+//   tipAmount.innerHTML = `$${tipTotal}`;
+// };
 
-const recalculateGrandTotal = () => {
-  const grandTotalValue = Math.round((initTotalValue + tipTotal) * 100) / 100;
-  grandTotal.innerHTML = `$${grandTotalValue}`;
-};
+// const recalculateGrandTotal = () => {
+//   const grandTotalValue = Math.round((initTotalValue + tipTotal) * 100) / 100;
+//   grandTotal.innerHTML = `$${grandTotalValue}`;
+// };
 
 const initTipCounter = () => {
   minus.addEventListener("click",() => {
     if(tipPercent !== 0) {
       minus.nextElementSibling.querySelector('input').stepDown(5);
       tipPercent -= 5;
-      recalculateTipTotal();
-      recalculateGrandTotal();
+// console.log(initTotalValue.toFormat());
+      // recalculateTipTotal();
+      // recalculateGrandTotal();
     };
   })
   plus.addEventListener("click",() => {
     plus.previousElementSibling.querySelector('input').stepUp(5);
     tipPercent += 5
-    recalculateTipTotal();
-    recalculateGrandTotal()
+// console.log(initTotalValue.toFormat());
+    // recalculateTipTotal();
+    // recalculateGrandTotal();
   })
 };
 
-export { initTipCounter }
+// export { initTipCounter }
 
 // .toFixed(2)
