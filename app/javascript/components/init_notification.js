4d3e1a0a722e5c9f68d70acad472ccfe81dbd98f
Pieter-Jan Caals <Pieter.Jan.Caals@gmail.com>
2019-03-07 15:15:38 -0500

    Some extra small fixes

--- app/javascript/components/init_notification.js
+++ app/javascript/components/init_notification.js
@@ -9,7 +9,7 @@ const initNotification = () =>{
 
   phone.addEventListener('click', () => {
     console.log('clicked');
-   notiContainer.innerHTML = "<div class='notification' id='notification'><img src=" + imageUrl + " alt='notification' class='notification-icon'><h6 class='notification-title'>KANGA</h6><p class='notification-text'> Your order is ready!</p><p class='notification-time'>1 min ago</p></div>"
+   notiContainer.innerHTML = "<div class='notification' id='notification'><img src=" + imageUrl + " alt='notification' class='notification-icon'><h6 class='notification-title'>KANGA</h6><p class='notification-text'> Your order is ready in 2min!</p><p class='notification-time'>Now</p></div>"
    notiIcon.innerHTML = "<div class='notification-symbol' id='notification_symbol'>1</div>"
   });
 
